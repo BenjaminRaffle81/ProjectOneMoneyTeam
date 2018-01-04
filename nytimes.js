51520b898bd8da664ccc1e9401281e6d56066cdc
Benjamin Raffle <benjamin.raffle@gmail.com>
2018-01-03 21:37:24 -0800

    Second commit

--- nytimes.js
+++ nytimes.js
@@ -1,36 +1,23 @@
+$("button").on("click", function() {
+   
+      var apiKey = "fecfa0341d564db69594e4b0f78fea8e"
+      var year = "2000";
+      var endYear = "2007";
+      var month = "dec";
 
-
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script type="text/javascript">
-    $("button").on("click", function() {
-      var person = $(this).attr("data-person");
-      var apikey = "fecfa0341d564db69594e4b0f78fea8e"
-
-      var queryURL = "http://api.nytimes.com/svc/archive/v1/" + year + "/" + month+ ".json?api-key=" + apiKey;
+      var queryURL = "https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=fecfa0341d564db69594e4b0f78fea8e";
 
       $.ajax({
           url: queryURL,
           method: "GET"
         })
+
         .done(function(response) {
-          var results = response.data;
+          var results = response;
+          console.log(results);
 
-          for (var i = 0; i < results.length; i++) {
-            var gifDiv = $("<div class='item'>");
-
-            var rating = results[i].rating;
-
-            var p = $("<p>").text("Rating: " + rating);
-
-            var personImage = $("<img>");
-            personImage.attr("src", results[i].images.fixed_height.url);
-
-            gifDiv.prepend(p);
-            gifDiv.prepend(personImage);
-
-            $("#gifs-appear-here").prepend(gifDiv);
-          }
+       
+          })
         });
-    });
-  </script>
+
 
